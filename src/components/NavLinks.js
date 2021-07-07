e68afd0a6dd894d6052c833eb0322c0c9343710a
Erich Purpur <epurpur@gmail.com>
2021-07-07 15:13:21 -0400

    dynamic underline on click

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -41,6 +41,7 @@ const NavLinks = () => {
             {/* Make list of HTML of each navbar item */}
             {navLinks.map((link) => 
             ( <li class="nav-item" onClick={() => toggleUnderline(link.id)}>
+            {/* Use conditional to evaluate if id.underlined === true. If so add underline tag. If not, no underline */}    
               <a class="nav-link active" aria-current="page" href="#">{(link.underlined ? <u>{link.title}</u>: `${link.title}` )}</a>
               </li> )
             )}
