5a1a722f234aba710efe51724134e0fe6a14240b
Erich Purpur <epurpur@gmail.com>
2021-07-08 10:04:37 -0400

    bringing in images from previous portfolio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
+
+  const [projects, setProjects] = useState([
+    {
+      id: 1,
+      title: 'OneFigr App',
+      active: true,
+    },
+    {
+      id: 2,
+      title: 'EcoValuator Plugin',
+      active: true,
+    },
+    {
+      id: 3,
+      title: 'Boone Real Estate',
+      active: true,
+    },
+    {
+      id: 4,
+      title: 'Light Cville',
+      active: true,
+    },
+    {
+      id: 5,
+      title: 'Is It Dry?',
+      active: true,
+    },
+    {
+      id: 6,
+      title: 'National Park Finder',
+      active: true,
+    },
+    {
+      id: 7,
+      title: 'Virginia River Monitor',
+      active: false
+    }
+  ])
+
 
   return (
     <div className="App">
       <Header />
+      <About />
       <Footer />
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div id='About'> 
+            <h1> About Me </h1>
+            <section id="AboutMeContainer">
+                <div id="AboutMeImg">
+                    <p>Img here</p>
+                </div>
+                <div id="AboutMeText">
+                    <p>Text here</p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default About
