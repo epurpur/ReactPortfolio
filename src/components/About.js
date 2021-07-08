fc55ae4ea0bfd83a2e0ee0225013e964d3162ce8
Erich Purpur <epurpur@gmail.com>
2021-07-08 10:19:37 -0400

    setting up css styles, fonts, etc

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,12 @@ const About = () => {
             <h1> About Me </h1>
             <section id="AboutMeContainer">
                 <div id="AboutMeImg">
-                    <p>Img here</p>
+                <img src="https://avatars.githubusercontent.com/u/36010345?s=460&u=10800388b8f1955e283ad5dec7a574f39e9215af&v=4" alt="Erich Purpur headshot"></img>
                 </div>
                 <div id="AboutMeText">
-                    <p>Text here</p>
+                <p>I'm a Research Librarian at the University of Virginia in Charlottesville, VA. I am looking to leverage my 
+                 Geographic Information System (GIS), data analysis, and web development skills together. I enjoy any project that challenges me but I love maps! I specialize in web mapping applications and data visualization.</p> 
+                <p>I grew up in Boone, NC. In my free time I love to be outside, especially rock climbing or skiing. I also like learning foreign languages, personal finance, and spending time with my girlfriend. I am a cat person and I don't need coffee to wake up early.</p>
                 </div>
             </section>
         </div>
