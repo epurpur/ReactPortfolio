b6d380bdf16f3e1a029d82c7bddb77b8eff25949
Erich Purpur <epurpur@gmail.com>
2021-07-12 21:36:45 -0400

    converting html to contactItems state w/ hooks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,25 +2,72 @@ import React, { useState } from 'react'
 
 const Contact = () => {
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
+    // set state variables for form input fields. These are updated 'onChange'
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
 
     const handleSubmit = (e) => {
-        e.preventDefault();
+        // handle press submit button
+        e.preventDefault()
 
+        // if I were to make a POST request to a Database it would happen here
         const userMessage = {name, email, message}
-        console.log(userMessage)
 
-        // alert('Your message has been submitted! Thank you for your patience while I return your inquiry.');
-        
+        alert('Your message has been submitted! Thank you for your patience while I return your inquiry.');
+    }
+
+    // set state variables for contact-info fields
+    const [contactItems, setContactItems] = useState([
+        {
+            id: 1,
+            title: 'Email',
+            href: 'mailto:epurpur@gmail.com',
+            imgSrc: '/images/email.png',
+            alt: 'email link with letter icon',
+            data: 'epurpur@gmail.com'
+        },
+        {
+            id: 2,
+            title: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/erich-purpur-140a1433/',
+            imgSrc: '/images/linkedin.png',
+            alt: 'LinkedIn logo',
+            data: 'LinkedIn Profile'
+        },
+        {
+            id: 3,
+            title: 'Github',
+            href: 'https://github.com/epurpur',
+            imgSrc: '/images/github.png',
+            alt: 'Github logo',
+            data: 'Github Profile'
+        },
+        {
+            id: 4,
+            title: 'Phone',
+            href: '',
+            imgSrc: '/images/phone.png',
+            alt: 'Old phone image',
+            data: '(828) 773 7140'
+        },
+    ])
+
+    const [hoverText, setHoverText] = useState('');
+
+    const showHoverText = () => {
+    // sets state of hoverText to item that user is hovering over
     }
 
     return (
         <div>
             <h1> Contact Me </h1>
-            <a href="mailto:epurpur@gmail.com" target="_blank"><img src={"/images/email.png"} alt="email link with letter icon" class="contact-item" data-label="epurpur@gmail.com"></a> 
-
+            <section id='contactItems'>
+                {/* Render each contact item */}
+                {contactInfo}
+            </section>
+            <br></br>
+            <p id='hoverText'>Here</p>
             <br></br>
             <h1> Or Send a Message </h1>
             <br></br>
