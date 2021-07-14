43f31a52b11fa9123dc5eaa421ae48b1ee3767eb
Erich Purpur <epurpur@gmail.com>
2021-07-14 16:00:46 -0400

    styling html info content as JSX in SingleProject component page

--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const SingleProject = ( {title, info, imgLink, deployLink} ) => {
-    
+const SingleProject = ( {title, info, imgLink, deployLink, githubLink} ) => {
     
 
     return (
@@ -13,14 +12,13 @@ const SingleProject = ( {title, info, imgLink, deployLink} ) => {
                     <img src={`/images/${imgLink}.png`} alt={`${title} application screenshot`}></img>
                 </div>
                 <div id='singleProjectInfo'>
-                    <div>{info}</div>
+                    <div id='singleProjectText' dangerouslySetInnerHTML={{__html: info}}></div>
                     <br></br>
                     <div id='projectLinks'>
-                        <a href={deployLink}><p>View Live</p></a>
-                        <p>View Github</p>
+                        <a href={deployLink} target="_blank"><p>View Live</p></a>
+                        <a href={githubLink} targer="_blank"><p>View Github</p></a>
                         <Link to='/Portfolio'><p> Back to Portfolio Page </p></Link>
                     </div>
-                    
                 </div>
             </div>
         </div>
