c298806938581c27185b8e67d2a0e7d3ff223a27
epurpur <epurpur@gmail.com>
2021-12-07 17:08:01 -0500

    updating About Me and Now sections

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = () => {
                 <p>I am a research librarian at the University of Virginia in Charlottesville, Virginia. I am looking to combine my 
                  Geographic Information System (GIS), data analysis, and web development skills. I specialize in web mapping applications and data visualization.</p> 
                 <br></br>
-                <p>I grew up in Boone, North Carolina. In my free time I love to be outside, especially rock climbing or skiing. I also like learning foreign languages, personal finance, and spending time with my girlfriend and her cat, Atticus.</p>
+                <p>I grew up in Boone, North Carolina. In my free time I love to be outside, especially rock climbing or skiing. I also like learning foreign languages, personal finance, yoga, cooking, and home improvement projects.</p>
                 </div>
             </section>
         </div>
--- src/components/Now.js
+++ src/components/Now.js
@@ -17,44 +17,57 @@ const Now = () => {
                 <h1 id='nowTitle'> Happening Now </h1>
                 <br></br>
                 <p> I was inspired to write about what is happening at the moment in my life by <a href="https://sive.rs/">Derek Sivers</a>, who makes a good case for doing it. </p>
-                <p>Last Updated: <span>September 6, 2021</span></p>
+                <p>Last Updated: <span>December 7, 2021</span></p>
             </section>
             <section id="nowCategories">
                 <table class="table">
                     <tbody>
                         <tr>
                             <th scope="row"><FontAwesomeIcon icon={faQuestion} size='2x'/></th>
-                            <td>I have been living the life of a bachelor since the end of August. My girlfriend Sarah is currently on a road trip before starting her new job as an Emergency Medicine physician in 
-                                Gallup, New Mexico! The plan is for me to eventually move out there to be with her but at the moment I am still in Charlottesville. The fall semester has started at UVA and the 
-                                beginning of each semester is always busy, especially the fall semester. We have also gone back to work in person at the University of Virginia. Naturally, there are some mixed feelings about this.
-                                I personally feel OK with it as all UVA students are required to be vaccinated and masks are required indoors. Though it is a bit concerning to see many thousands of people crowding into
-                                college football stadiums around the country with little or no mask wearing.  
+                            <td>Life has certainly changed since my last post. My girlfriend and I broke up at the end of the summer so I am living the bachelor life at the moment. I have
+                                been thinking hard about taking an extended hiatus away from dating. A relationship is demanding and it is not until you step away from it that you realize 
+                                it. Nevertheless, quite a few other things have been happening in my life since then. My friend Hilary is living with me for the winter and it has been very
+                                nice so far! I have been starting some home projects. Now that I'll be staying in Charlottesville I have some updates I want to do to the house, one of which
+                                will be to paint the downstairs this winter. I have some more ambitious plans I may or may not do as well.
                             </td>
                         </tr>
                         <br></br>
                         <tr>
                             <th scope="row"><FontAwesomeIcon icon={faSkiing} size='2x'/></th>
-                            <td>Late summer in Virginia is a sweaty time but just before Labor Day weekend we got a brief reprieve from the slime. I went home to Boone, NC for the weekend but spent a day climbing at <a href="https://www.dcr.virginia.gov/state-parks/grayson-highlands">Grayson Highlands State Park</a> on the way there.
-                            It felt so nice to get outdoors climbing as I have not had a chance to do it much lately. I am looking forward to the fall so I can get out more often!</td>
+                            <td>I have been able to climb a lot this fall, which I really appreciate. I did not go into the fall with any particular projects and feel my performance has been
+                                a little lackluster because of that. I've climbed around locally on the Blue Ridge Parkway a bit, doing some new routes and boulder problems and repeating others.
+                                I took a week's trip to the Obed in Tennessee, an area I had not really been to much before. That place was great! I also went to the New River Gorge for a couple
+                                weekends and always really enjoy my time there. I have some extra time off around Christmas and New Years time so I am thinking I'll follow the weather and try to
+                                go on a trip to North Carolina or the Chattanooga area. 
+                            </td>
                         </tr>
                         <br></br>
                         <tr>
                             <th scope="row"><FontAwesomeIcon icon={faDesktop} size='2x'/></th>
-                            <td>An upcoming side project for me will be for our group to update the <a href="http://www.keylogeconomics.com/ecovaluator.html">EcoValuator</a> Plugin. This is an ongoing project I have worked on over the last several years with <a href="http://www.keylogeconomics.com/">Key-Log Economics</a>.
-                            This project is a 3rd party plugin to the <a href="https://www.qgis.org/en/site/">QGIS</a> software and predicts the value of land for various use cases throughout North America. The plan in the future is to make the plugin more robust and also allow for use outside North America.
-                            I think Southeast Asia is the next region? Vietnam in particular. This is because Key-Log Economics has expanded work into that region. I am meeting later this week with Key-Log so will have more details coming soon.</td>
+                            <td> This fall I have not been programming every day as I was earlier this year. I have done some small projects, mostly in python. The most exciting one was a data wrangling project. 
+                                A UVA professor I have been working with has student evaluation data from the classes she is teaching which she wants to pivot into a more easily readable format. It was a tricky 
+                                exercise to figure out how to re-arrange the data she was working with but we got it mostly figured out. A fun upcoming project I have started on a little bit will be 'Greenstonia'.
+                                This will be similar to <a href="https://www.mountainproject.com/">Mountain Project</a>, specifically tailored for the local bouldering on the Blue Ridge Parkway. There
+                                was a .pdf guide available on line 8 or 10 years ago but it is out of date and now is unavailable offline. In order to keep my coding chops up and have some fun, I want to re-do this
+                                in a modern way. It will be a MERN stack application.
+                            </td>
                         </tr>
                         <br></br>
                         <tr>
                             <th scope="row"><FontAwesomeIcon icon={faLanguage} size='2x'/></th>
-                            <td>Language Learning has been mostly on hold for a while. I just have devoted much energy to it lately. I am happy to at least have maintained my practice of reading the news in Spanish for 10-15 minutes on <a href="https://www.euronews.com/">Euronews</a>.</td>
+                            <td>Language Learning has been mostly on hold for a while. I just have devoted much energy to it lately. I am happy to at least have maintained my practice of reading the news in Spanish 
+                                for 10-15 minutes on <a href="https://www.euronews.com/">Euronews</a>.
+                            </td>
                         </tr>
                         <br></br>
                         <tr>
                             <th scope="row"><FontAwesomeIcon icon={faBook} size='2x'/></th>
-                            <td>Currently I am reading <a href="https://www.amazon.com/Bury-My-Heart-Wounded-Knee/dp/0805086846">Bury My Heart at Wounded Knee</a> by Dee Brown. It is a tale about the systematic destruction of the large Native American tribes west of the Mississippi River in the mid to late 1800s. 
-                            As I'll be moving to New Mexico to be with Sarah sometime in the not so distance future, I'll be living in the heart of native american country and I want to understand a little bit more about these people. While the book is depressing it is also very interesting to learn about these people who 
-                            were a part of the great nations who once lived in the western United States. Though it makes me feel ashamed in many ways to be an american, this is a highly recommended read!</td>
+                            <td>Currently I am reading two books. One is <a href="https://www.amazon.com/Dune-Chronicles-Book-1/dp/0441013597/ref=sr_1_3?keywords=Dune&qid=1638914562&sr=8-3">Dune</a> by Frank Herbert. 
+                            I was inspired to finally read this after the recent movie came out. The other I am re-reading is <a href="https://www.amazon.com/Guide-Good-Life-Ancient-Stoic/dp/0195374614/ref=sr_1_1?keywords=a+guide+to+the+good+life&qid=1638914664&sr=8-1">
+                            A Guide to the Good Life</a> by William Irvine. It is about the practices of the ancient stoics such as Seneca and Marcus Aurelius. Basically, it is a book about how to create a philosophy
+                            of life for yourself. It is recommended by <a href="https://www.mrmoneymustache.com/">Mr. Money Moustache</a> and many other personal heros of mine. I am re-reading this because it is a lot to digest
+                            but the gist of it is to create feelings of happiness in the life you are living. 
+                            </td>
                         </tr>
                     </tbody>
                 </table>
